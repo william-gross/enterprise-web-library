56f8d16122e8c2543b846c2fd1d63a5577efd5ba
Sam Rueby <samrueby@gmail.com>
2019-01-07 14:05:30 -0500

    ExportEwlToLocalFeed can now optionally export to the non-default folder.

--- Development Utility/Operations/ExportEwlToLocalFeed.cs
+++ Development Utility/Operations/ExportEwlToLocalFeed.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq;
 using RedStapler.StandardLibrary;
 using RedStapler.StandardLibrary.Configuration;
 using RedStapler.StandardLibrary.InstallationSupportUtility;
@@ -7,19 +9,32 @@ using RedStapler.StandardLibrary.InstallationSupportUtility.InstallationModel;
 namespace EnterpriseWebLibrary.DevelopmentUtility.Operations {
 	internal class ExportEwlToLocalFeed: Operation {
 		public static Operation Instance { get; } = new ExportEwlToLocalFeed();
+		private string outputPath;
+
 		private ExportEwlToLocalFeed() { }
 
-		bool Operation.IsValid( Installation genericInstallation ) =>
-			genericInstallation is RecognizedDevelopmentInstallation installation && installation.DevelopmentInstallationLogic.SystemIsEwl;
+		bool Operation.IsValid( Installation genericInstallation ) {
+			var isEwl = genericInstallation is RecognizedDevelopmentInstallation installation && installation.DevelopmentInstallationLogic.SystemIsEwl;
+			if( !isEwl )
+				return false;
+
+			var commandLine = Environment.GetCommandLineArgs();
+
+			if( commandLine.Length < 3 )
+				outputPath = StandardLibraryMethods.CombinePaths( ConfigurationStatics.RedStaplerFolderPath, "Local NuGet Feed" );
+			else
+				outputPath = commandLine.Last();
+
+			return true;
+		}
 
 		void Operation.Execute( Installation genericInstallation, OperationResult operationResult ) {
 			var installation = genericInstallation as RecognizedDevelopmentInstallation;
-			var localNuGetFeedFolderPath = StandardLibraryMethods.CombinePaths( ConfigurationStatics.RedStaplerFolderPath, "Local NuGet Feed" );
 
 			// NuGet.exe has problems if the folder doesn't exist.
-			Directory.CreateDirectory( localNuGetFeedFolderPath );
+			Directory.CreateDirectory( outputPath );
 
-			ExportLogic.CreateEwlNuGetPackage( installation, true, localNuGetFeedFolderPath, true );
+			ExportLogic.CreateEwlNuGetPackage( installation, true, outputPath, true );
 		}
 	}
 }
