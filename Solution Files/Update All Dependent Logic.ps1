c705319b1e75d057351e455782425c25606adb0e
Sam Rueby <samrueby@gmail.com>
2019-01-07 13:41:18 -0500

    Improved Update All Dependent Logic.ps1 to help debugging.

--- Solution Files/Update All Dependent Logic.ps1
+++ Solution Files/Update All Dependent Logic.ps1
@@ -1,12 +1,45 @@
+$ErrorActionPreference = "Stop"
+
+function getCurrentPath {
+    (Resolve-Path .\).Path
+}
+
+echo "Current Directory is $(getCurrentPath)"
 cd ..
-if( Test-Path packages\EWL ) { Remove-Item packages\EWL -Recurse }
+echo "Current Directory is now $(getCurrentPath)"
+
+$ewlPackageFolderPath = "packages\EWL"
+echo "Checking if $ewlPackageFolderPath exists";
+if( Test-Path $ewlPackageFolderPath ) { 
+    echo "Exists. Removing folder."
+	Remove-Item $ewlPackageFolderPath -Recurse 
+}
 
 $packageIdFilePath = "Library\Configuration\EWL Package Id.txt"
-$packageId = if( Test-Path $packageIdFilePath ) { Get-Content $packageIdFilePath } else { "Ewl" }
+echo "`$packageIdFilePath=$packageIdFilePath"
+
+$packageId = if( Test-Path $packageIdFilePath ) { 
+    Get-Content $packageIdFilePath } else { 
+    "Ewl" 
+}
+echo "`$packageId=$packageId"
+
 $searchTerm = if( $packageId -eq "Ewl" ) { "PackageId:$packageId" } else { $packageId }
-$packageVersion = ( ( & "Solution Files\nuget" list $searchTerm -Prerelease -NonInteractive ) | Select-String $packageId -SimpleMatch | Select-Object -First 1 ).Line.Split( " " )[1]
+echo "`$searchTerm=$searchTerm"
+
+$packageVersion = "2019.1.8.101427-pr";
+echo "`$packageVersion=$packageVersion"
+
+echo "Creating a packages.config"
 New-Item packages\EWL\packages.config -Force -ItemType file -Value "<?xml version=`"1.0`" encoding=`"utf-8`"?><packages><package id=`"$packageId`" version=`"$packageVersion`" /></packages>" | Out-Null
+
+echo "Running nuget restore"
 & "Solution Files\nuget" restore packages\EWL\packages.config -PackagesDirectory packages\EWL -NonInteractive
 
 cd "packages\EWL\Ewl*\Development Utility"
+echo "Current Directory is now $(getCurrentPath)"
+
+echo "Running UpdateAllDependentLogic"
 & .\EnterpriseWebLibrary.DevelopmentUtility ..\..\..\.. UpdateAllDependentLogic
+
+echo "Done."
