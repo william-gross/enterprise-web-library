9855fa4ae577fe3d41e15b7b95a3bac8f67062ab
Sam Rueby <samrueby@gmail.com>
2019-01-08 10:52:22 -0500

    Update package version.

--- Solution Files/Update All Dependent Logic.ps1
+++ Solution Files/Update All Dependent Logic.ps1
@@ -27,7 +27,7 @@ echo "`$packageId=$packageId"
 $searchTerm = if( $packageId -eq "Ewl" ) { "PackageId:$packageId" } else { $packageId }
 echo "`$searchTerm=$searchTerm"
 
-$packageVersion = "2019.1.8.101427-pr";
+$packageVersion = "2019.1.8.110110";
 echo "`$packageVersion=$packageVersion"
 
 echo "Creating a packages.config"
